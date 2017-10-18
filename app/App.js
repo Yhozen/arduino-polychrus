31c8760a451f7a60d4c5f5b216214cf9131ce0d9
Gabriel <gabriel@garox.org>
2017-10-18 16:38:52 -0300

    init

---
+++ app/App.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  render() {
+    const resizeMode = 'view'
+    return (
+      <ImageBackground
+  style={{
+    backgroundColor: '#ccc',
+    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  }}
+  source={require('./images/bg.jpg')}
+>
+  <Text
+    style={{
+      backgroundColor: 'transparent',
+      textAlign: 'center',
+      fontSize: 30,
+      color: 'white',
+      padding: 40,
+    }}
+  >
+    Polychrus Marmoratus
+  </Text>
+</ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
