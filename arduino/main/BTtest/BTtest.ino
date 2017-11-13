9e44cad12a59a7aef25cc90cef39a14404c1c81e
Gabriel <gabriel@garox.org>
2017-11-13 18:20:50 -0300

    bt

---
+++ arduino/main/BTtest/BTtest.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT(0, 1);
+
+void setup() {
+  BT.begin(9600);
+  BT.println("Bluetooth Ready.");
+  BT.println("Waiting...");
+  pinMode(13, OUTPUT);
+  delay(1000);
+}
+
+void loop() {
+  String readByte;
+  boolean readSomething = false;
+  BT.listen();
+  while (BT.available() > 0) {
+    readByte = BT.readString();
+    readSomething = true;
+  }
+  delay(50);
+  if (readSomething == true) {
+    delay(20);
+    readSomething = false;
+    BT.println(readByte);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+  }
+}
